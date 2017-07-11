c54b2affa794fca74ea79a3bedd652a27cd272cc
Chris Till <cpaultill@gmail.com>
2017-07-11 10:32:52 -0400

    proper api usage lul

--- overtube/overtube.py
+++ overtube/overtube.py
@@ -64,7 +64,7 @@ class Overtube:
                             developerKey=self.DEVELOPER_KEY)
 
             ytchannel = youtube.channels().list(part='snippet,contentDetails', id="UClOf1XXinvZsy4wKPAkro2A").execute()
-            uploadPL = ytchannel['contentDetails']['relatedPlaylists']['uploads']
+            uploadPL = ytchannel['items']['contentDetails']['relatedPlaylists']['uploads']
             if uploadPL.pageInfo.totalResults != self.uploads:
                 print("New video from PlayOverwatch")
 
